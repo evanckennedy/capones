91e61a9e44a58ff3d1955ae8cc76d871aa4e1dd8
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-21 10:56:02 -0500

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './style/reset.css';
+import './style/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
