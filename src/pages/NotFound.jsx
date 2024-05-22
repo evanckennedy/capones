10b079bc0d2fb08026a192d52cdf98851a4220f5
LL-fullstack <luliuszwg@gmail.com>
2024-05-21 21:18:03 -0500

    Added error component - Lu

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,18 @@
+import errorImage from '../media/errorimg.png';
+
 function NotFound() {
   return (
-    <>
-      <div>notfound</div>
-    </>
+    <section className="not-found">
+      <img src={errorImage} alt='not-found-img' class="not-found-img" />
+      <div className="page-not-found">
+        <h1 className="not-found-title">Page not found</h1>
+        <p>Sorry, we couldn't find the page you were looking for. 
+           We suggest that you return to home page.
+        </p>
+        <button className="not-found-btn">Go to Homepage</button>
+      </div>
+    </section>
   )
 }
 
-export default NotFound
+export default NotFound;
