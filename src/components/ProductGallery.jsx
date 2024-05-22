b8a9386adea9cc2213b68c922d823deca02ed888
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-22 10:51:03 -0500

    use defaultValue prop for select element

--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -1,11 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function ProductGallery() {
+  const [product, setProducts] = useState([]);
+
+  const URL ='https://fakestoreapi.com/products';
+
+  /* useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await axios.get(URL);
+        console.log(response)
+      } catch (error) {
+        
+      }
+    }
+  }, []) */
+
   return (
     <>
       <div className="product-toolbar flex justify-between">
         <h3>New Collection</h3>
         <div className="custom-select">
-          <select className="sort-button">
-            <option disabled selected>Sort: Best Match</option>
+          <select className="sort-button" defaultValue="Sort: Best Match">
+            <option>Sort: Best Match</option>
             <option>Lowest Price</option>
             <option>Highest Price</option>
             <option>Price + Shipping: lowest first</option>
