6f2f38d8e5d724e88a18487549460a3128108c46
LL-fullstack <luliuszwg@gmail.com>
2024-05-22 18:47:32 -0500

    Added CartProduct class for random Items - Lu

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,49 +1,100 @@
 import { FaRegTrashCan } from "react-icons/fa6";
 import clothSample from "../media/clothSample.webp";
+import CartProduct from "../utils/CartProduct";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Cart() {
+
+  const [cartProducts, setCartProducts] = useState([]);
+  const numberOfCartItems = 4;
+
+  const URL ='https://fakestoreapi.com/products';
+  useEffect(() => {
+    async function getCartProducts() {
+      try {
+        const response = await axios.get(URL);
+        const data = response.data;
+        console.log(
+          data
+        );
+        const cartProductList = data.map(product => 
+          new CartProduct(product.id, product.title, product.price, product.image, 1)
+        )
+        const filteredCartItems = cartProductList.slice(0,numberOfCartItems);
+        setCartProducts(filteredCartItems);
+        console.log(
+          filteredCartItems
+        );
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    getCartProducts();
+  }, []);
+
+  const handleQuantityChange = (productId, newQuantity) => {
+    const updatedCartProducts = cartProducts.map(product => {
+      if (product.id === productId) {
+        product.updateQuantity(newQuantity);
+      }
+      return product;
+    });
+    setCartProducts(updatedCartProducts);
+  };
+  
+
   return (
     <div className="cart-container">
       <h1 className="cart-title">Shopping Bag</h1>
-      <h2 className="cart-subtitle">3 items</h2>
+      <h2 className="cart-subtitle">{numberOfCartItems} items</h2>
       <div className="cart-checkout-section">
-        <div className="cart-item">
-          <img src={clothSample} alt="product-img" className="cart-item-img" />
-          <table className="cart-items-table">
-            <thead>
-              <tr className="first-row">
-                <th>Rain Jacket Women Windbreaker Striped</th>
-                <th>Each</th>
-                <th>Quantity</th>
-                <th>Total</th>
-                <th><FaRegTrashCan /></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="second-row">
-                <td>XS | WHITE</td>
-                <td>$23.99</td>
-                <td>
-                  <select className="quantity-select">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                  </select>
-                </td>
-                <td>$23.99</td>
-              </tr>
-              <tr className="third-row">
-                <td>UPC: 29436937</td>
-              </tr>
-              <tr className="fourth-row">
-                <td>In Stock</td>
-              </tr >
-              <tr className="fifth-row">
-                <td><span className="edit">Edit</span></td>
-              </tr>
-            </tbody>      
-          </table>
-        </div>
+        {
+          cartProducts.map( (product) => 
+            <div className="cart-item">
+            <img src={product.image} alt="Product Image" className="cart-item-img" />
+            <table className="cart-items-table">
+              <thead>
+                <tr className="first-row">
+                  <th className="first-row-header">{ product.title }</th>
+                  <th>Each</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                  <th><FaRegTrashCan /></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="second-row">
+                  <td>{ product.size } | { product.color }</td>
+                  <td>$ { product.price }</td>
+                  <td>
+                    <select 
+                    className="quantity-select"
+                    value={product.quantity} 
+                    onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value))}
+                    >
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                    </select>
+                  </td>
+                  <td>${ product.totalPrice } </td>
+                </tr>
+                <tr className="third-row">
+                  <td>UPC: { product.upc }</td>
+                </tr>
+                <tr className="fourth-row">
+                  <td>In Stock</td>
+                </tr >
+                <tr className="fifth-row">
+                  <td><span className="edit">Edit</span></td>
+                </tr>
+              </tbody>      
+            </table>
+          </div>
+          )
+        }
       </div>
       
     </div>
