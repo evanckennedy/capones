ff773a2cfbe13db95251783878c27f928b3a2393
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-23 10:43:17 -0500

    add scroll-behaviour smooth to html

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,7 +3,7 @@ function HeroBanner() {
     <div className='container'>
       <div className="hero-banner">
         <h2 className="hero-banner-text">PRE-FALL 2024</h2>
-        <button>Shop now</button>
+        <a href="#product-gallery-section"><button>Shop now</button></a>
       </div>
     </div>
   )
