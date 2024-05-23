5ec660187aba8fa5859489efec0ab167dcfbf783
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-23 10:37:14 -0500

    clean up hero banner code

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,6 @@
 function HeroBanner() {
   return (
-    <div className=' container'>
+    <div className='container'>
       <div className="hero-banner">
         <h2 className="hero-banner-text">PRE-FALL 2024</h2>
         <button>Shop now</button>
