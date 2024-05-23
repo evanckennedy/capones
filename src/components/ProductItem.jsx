620caa8d5c3800984327006424b54a59b7d7bc98
gl-school <gabriellinski@student.mitt.ca>
2024-05-22 21:57:50 -0500

    animations

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,12 +5,12 @@ function ProductItem({ product }) {
     <Link to={`/product/${product.id}`}>
       <div className="grid-item flex">
         <figure>
-          <img src={product.image} alt={product.title} />
+          <img className="gallery-image" src={product.image} alt={product.title} />
         </figure>
         <div className="grid-product-info flex">
           <p>{product.title}</p>
           <p>{product.category}</p>
-          <p>$ {product.price.toFixed(2)}</p>
+          <p>${product.price.toFixed(2)}</p>
         </div>
       </div>
     </Link>
