4c0db27066dd7d4476db347e21c56d7945c3de68
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-23 10:36:39 -0500

    remove unused code in hero-banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,23 +1,9 @@
-
-
-import { useNavigate } from 'react-router-dom';
-
-
 function HeroBanner() {
-
-
-  const navigate = useNavigate();
-  function redirect(location) {
-    navigate(`/${location}`);
-  };
-  
-
   return (
     <div className=' container'>
       <div className="hero-banner">
         <h2 className="hero-banner-text">PRE-FALL 2024</h2>
-        <button onClick={() => redirect('')}>Shop now</button>
-
+        <button>Shop now</button>
       </div>
     </div>
   )
