7495e737c2f159fa8fccffd164848e4cc558f0ec
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-24 09:24:19 -0500

    add framer motion smooth transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useRoutes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
   const location = useLocation();
@@ -17,15 +17,46 @@ function App() {
     location.pathname === '/cart'
   );
 
+  const pageVariants = {
+    initial: {
+      opacity: 0
+    },
+    in: {
+      opacity: 1
+    },
+    out: {
+      opacity: 0
+    }
+  };
+
+  const pageTransition = {
+    type: "tween",
+    ease: "anticipate",
+    duration: 0.5
+  };
+
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/product/:slug', element: <Product /> },
+    { path: '/cart', element: <Cart /> },
+    { path: '*', element: <NotFound /> }
+  ]);
+
   return (
     <>
       {!isNotFound && <Header />}
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/product/:slug' element={<Product />} />
-        <Route exact path='/cart' element={<Cart />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.key}
+          initial="initial"
+          animate="in"
+          exit="out"
+          variants={pageVariants}
+          transition={pageTransition}
+        >
+          {routes}
+        </motion.div>
+      </AnimatePresence>
       {!isNotFound && <Footer />}
     </>
   );
